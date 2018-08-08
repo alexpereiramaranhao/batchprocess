8600e85522516089a1e2e639e2b89563fa1b6a4a
alex <alex@alex-mint>
2018-08-08 01:25:48 -0300

    início do projeto

---
+++ batchprocess/src/main/java/br/com/mpx/batchprocess/BatchprocessApplication.java
@@ -0,0 +1,12 @@
+package br.com.mpx.batchprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchprocessApplication.class, args);
+	}
+}
